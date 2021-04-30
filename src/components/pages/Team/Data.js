84fa1e1d6c1e50968cf773f62140377d00ddda1b
amartsop <amartsop@gmail.com>
2021-04-30 20:39:49 +0100

    third

--- src/components/pages/Team/Data.js
+++ src/components/pages/Team/Data.js
@@ -1,6 +1,6 @@
 export const teamMember1 = {
     name: 'Dabal Pedamonti',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam purus ligula, auctor et ante sit amet, condimentum pretium justo. Vestibulum et lobortis risus. Duis semper odio dolor, eu porta erat suscipit vel. Fusce egestas ex varius lacus lobortis, quis lacinia lectus faucibus. Fusce posuere vulputate sapien sed iaculis. Ut posuere, quam a finibus egestas, nibh est ultricies nulla, nec congue ipsum est sit amet diam. Aliquam nec consectetur ante, at dignissim nulla. Nam sit amet convallis velit, vitae blandit diam.",
+    description: "Dabal studied Electronics and Computer Science (MEng) at the University of Edinburgh where he focused on machine learning, robotics and autonomous systems as well as parallel architectures and software in the embedded systems relevant for the Internet of Things. His PhD research lies at the intersections of deep reinforcement learning, robotics, and neuroscience by utilising simulation to speed up training time of the physical robot.  Dabal completed an enterprise module that provided business skills and a background understanding of business principles. He will be responsible for coordinating the finance of the company as well as leading the software development by working on the main algorithm and the simulation of the cloth deformations on the avatar.",
     img: "/images/TeamImgs/Dabal.jpg"
 };
 
@@ -17,3 +17,4 @@ export const teamMember3 = {
     description: "Athanasios has a Master’s degree in Mechanical Engineering from the Aristotle University of Thessaloniki and years of work experience in computational dynamics and control systems. His PhD focuses on the real-time simulation of soft bodies, emphasizing on aspects of accuracy and computational efficiency. He has also worked extensively on high performance computing technologies for scientific applications, photogrammetry, computer vision and 3D computer graphics for both desktop and web platforms. Furthermore, Athanasios has experience with web and native mobile application development. He will be responsible for the development of part of the main algorithms, such as the automated rigging, the skeletal animation, the 3D scanning and the real-time cloth/avatar simulation.",
     img: "/images/TeamImgs/Thanos.jpg"
 };
+
